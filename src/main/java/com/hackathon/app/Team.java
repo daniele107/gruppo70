95b0fa23b2305049f8d9f5356ecb7bcee68c020b
simoiod <simo.iodice@studenti.unina.it>
2025-04-22 14:33:36 +0200

    Update Team.java

--- src/main/java/com/hackathon/app/Team.java
+++ src/main/java/com/hackathon/app/Team.java
@@ -1,7 +1,14 @@
 package com.hackathon.app;
 import java.util.List;
-public class Team {
-    private String nome_team;
-    protected List<Partecipante> partecipanti;
 
+public class Team {
+    private String nome;
+    private List<Partecipante> partecipanti;
+
+    public Team Team(nome String, partecipanti List<Partecipanti>);
+
+    private String getNome();
+    private List<Partecipante> getPartecipanti();
+    private void setPartecipanti(partecipanti List<Partecipanti>)
+    private void setNome(nome String);
 }
