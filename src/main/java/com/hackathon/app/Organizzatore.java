b702ee5e32d5fdf9c2a28c9a32027e5c5faf5413
simoiod <simo.iodice@studenti.unina.it>
2025-04-22 14:46:55 +0200

    Update Organizzatore.java

--- src/main/java/com/hackathon/app/Organizzatore.java
+++ src/main/java/com/hackathon/app/Organizzatore.java
@@ -4,17 +4,7 @@ package com.hackathon.app;
 import java.time.LocalDate;
 
 public class Organizzatore extends Utente {
-  /*  public LocalDate AperturaIscrizioni(data LocalDate) {
-        return null;
-    }
-
-    public LocalDate ChiusuraIscrizioni(data LocalDate) {
-        return null;
-    }
-
-    public String Invito(email String) {
-        return null;
-    }
-
-   */
+  public LocalDate AperturaIscrizioni(LocalDate data);
+  public LocalDate ChiusuraIscrizioni(LocalDate data);
+  public String Invito(String email);
 }
