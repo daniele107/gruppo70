debece18c6cd9d9a1b14ef31d88d53dec9aee738
simoiod <simo.iodice@studenti.unina.it>
2025-04-22 14:50:52 +0200

    Update Utente.java

--- src/main/java/com/hackathon/app/Utente.java
+++ src/main/java/com/hackathon/app/Utente.java
@@ -3,25 +3,25 @@ package com.hackathon.app;
 import java.time.LocalDate;
 
 public class Utente {
-    private String nome;
-    private String cognome;
-    private LocalDate dataNascita;
-    private String email;
-    private String password;
+  private String nome;
+  private String cognome;
+  private LocalDate dataNascita;
+  private String email;
+  private String password;
 
-    public Utente(String nome, String cognome, LocalDate dataNascita, String email, String passowrd);
+  public Utente(String nome, String cognome, LocalDate dataNascita, String email, String passowrd);
     
-    public Utente Registrazione(String nome, String cognome, LocalDate dataNascita, String email, String password);
-    public Utente SignIn(String email, String password);
+  public Utente Registrazione(String nome, String cognome, LocalDate dataNascita, String email, String password);
+  public Utente SignIn(String email, String password);
 
-    public String getNome();
-    public void setNome(String nome);
-    public String getCognome();
-    public void setCognome(String cognome);
-    public LocalDate getDataNascita();
-    public void setDataNascita(LocalDate dataNascita);
-    public String getEmail();
-    public void setEmail(String email);
-    public String getPassword();
-    public void setPassword(String password);
+  public String getNome();
+  public void setNome(String nome);
+  public String getCognome();
+  public void setCognome(String cognome);
+  public LocalDate getDataNascita();
+  public void setDataNascita(LocalDate dataNascita);
+  public String getEmail();
+  public void setEmail(String email);
+  public String getPassword();
+  public void setPassword(String password);
 }
