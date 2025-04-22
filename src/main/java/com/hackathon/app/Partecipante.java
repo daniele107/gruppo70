c0658a2d8bc42dd678ab4ec9d41390b25cd3ed27
simoiod <simo.iodice@studenti.unina.it>
2025-04-22 14:45:03 +0200

    Update Partecipante.java

--- src/main/java/com/hackathon/app/Partecipante.java
+++ src/main/java/com/hackathon/app/Partecipante.java
@@ -2,6 +2,4 @@ package com.hackathon.app;
 
 public class Partecipante extends Utente {
     public Invito Invita(email String);
-    public String AccettaInvito();
-    public String RifiutaInvito();
 }
