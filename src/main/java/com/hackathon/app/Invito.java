e806d8dca0cd8c88ec21b64d720fe437da849372
simoiod <simo.iodice@studenti.unina.it>
2025-04-22 14:45:35 +0200

    Update Invito.java

--- src/main/java/com/hackathon/app/Invito.java
+++ src/main/java/com/hackathon/app/Invito.java
@@ -1,1 +1,15 @@
+package com.hackathon.app;
 
+public class Invito {
+  private String messaggio;
+  private LocalDate dataInvio;
+  
+  public Invito(String messaggio, LocalDate dataInvio);
+  public String getMessaggio();
+  public void setMessaggio(String messaggio);
+  public LocalDate getDataInvio();
+  public void setDataInvio(LocalDate dataInvio);
+  
+  public String AccettaInvito();
+  public String RifiutaInvito();
+}
