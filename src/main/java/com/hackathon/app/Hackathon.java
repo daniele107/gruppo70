077f63dbe900a32dd1993a971e24faeb0255740f
simoiod <simo.iodice@studenti.unina.it>
2025-04-22 14:51:39 +0200

    Update Hackathon.java

--- src/main/java/com/hackathon/app/Hackathon.java
+++ src/main/java/com/hackathon/app/Hackathon.java
@@ -3,26 +3,26 @@ package com.hackathon.app;
 import java.time.LocalDate;
 
 public class Hackathon {
-    private String titolo;
-    private String sede;
-    private LocalDate dataInizio;
-    private LocalDate dataFine;
-    private int massimoPartecipanti;
-    private int dimensioneTeam;
+  private String titolo;
+  private String sede;
+  private LocalDate dataInizio;
+  private LocalDate dataFine;
+  private int massimoPartecipanti;
+  private int dimensioneTeam;
 
-    public Hackathon(String titolo, String sede, LocalDate dataInizio, LocalDate dataFine, int massimoPartecipanti, int dimensioneTeam);
-    public String getTitolo();
-    public void setTitolo(String titolo);
-    public String getSede();
-    public void setSede(String sede);
-    public LocalDate getDataInizio();
-    public void setDataInizio(LocalDate dataInizio);
-    public LocalDate getDataFine();
-    public void setDataFine(LocalDate dataFine);
-    public int getMassimoPartecipanti();
-    public void setMassimoPartecipanti(int massimoPartecipanti);
-    public int getDimensioneTeam();
-    public void setDimensioneTeam(int dimensioneTeam);
+  public Hackathon(String titolo, String sede, LocalDate dataInizio, LocalDate dataFine, int massimoPartecipanti, int dimensioneTeam);
+  public String getTitolo();
+  public void setTitolo(String titolo);
+  public String getSede();
+  public void setSede(String sede);
+  public LocalDate getDataInizio();
+  public void setDataInizio(LocalDate dataInizio);
+  public LocalDate getDataFine();
+  public void setDataFine(LocalDate dataFine);
+  public int getMassimoPartecipanti();
+  public void setMassimoPartecipanti(int massimoPartecipanti);
+  public int getDimensioneTeam();
+  public void setDimensioneTeam(int dimensioneTeam);
     
-    public static void main(String[] args);
+  public static void main(String[] args);
 }
