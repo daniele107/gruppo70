e98316a53aa07075d870517de07b825dd5f3d51d
simoiod <simo.iodice@studenti.unina.it>
2025-04-22 14:51:05 +0200

    Update Voto.java

--- src/main/java/com/hackathon/app/Voto.java
+++ src/main/java/com/hackathon/app/Voto.java
@@ -1,9 +1,9 @@
 package com.hackathon.app;
 
 public class Voto {
-    private int voto;
+  private int voto;
     
-    public Voto(int voto);
-    public int getVoto();
-    public void setVoto(int voto);
+  public Voto(int voto);
+  public int getVoto();
+  public void setVoto(int voto);
 }
