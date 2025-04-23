678bd6b398dded38b441a047a461acdc05339a8b
daniele107 <danielemegna10@gmail.com>
2025-04-23 21:07:15 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/hackathon/app/Invito.java

---
+++ src/main/java/model/Main.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Utente a = new Giudice();
+             a.nome="Marco";
+             System.out.println(a.nome);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci un nome");
+        a.nome =scanner.nextLine();
+        System.out.println(a.nome);
+        Team ercole = new Team();
+          ercole.nome_team="forza napoli";
+        System.out.println(ercole.nome_team);
+
+        Voto b = new Voto();
+        b.voto=30;
+        System.out.println("Risultato: " + b.controllo_voto(b.voto));
+    }
+}
